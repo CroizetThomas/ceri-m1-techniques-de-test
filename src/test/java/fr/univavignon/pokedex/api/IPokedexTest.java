f52929cf638db06105eeedfe003b54d356665250
CroizetThomas <thom.croiz18@gmail.com>
2024-03-05 10:29:44 +0100

    Implementation Pokedex test 2

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -16,14 +16,11 @@ public class IPokedexTest {
 
 
     @Before
-    public void setUp() throws PokedexException {
+    public void setUp() {
         pokedex = new Pokedex();
 
         pokemonBulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
         pokemonAquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
-
-        pokedex.addPokemon(pokemonBulbizarre);
-        pokedex.addPokemon(pokemonAquali);
     }
 
 
